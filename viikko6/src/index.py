d6a2ef3419133e474d4d1a87f2264687783a683a
Veeti Hytönen <veeti.hytonen@gmail.com>
2024-12-05 16:58:00 +0200

    ex. 3

--- viikko6/src/index.py
+++ viikko6/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -8,41 +8,16 @@ def main():
     stats = Statistics(reader)
 
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
         print(player)
-
-    print()
-    print()
-
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-    
-    print()
-    print()
-
-    matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-
-    print()
-    print()
-
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":
--- viikko6/src/matchers.py
+++ viikko6/src/matchers.py
@@ -9,6 +9,16 @@ class And:
 
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
 
 class PlaysIn:
     def __init__(self, team):
